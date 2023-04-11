aaf4465dd225b682c5e7ffe122b5a3339ff7b4f2
Sarra <sarra.sta@esprit.tn>
2023-04-11 04:19:09 +0100

    Third commit main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
-abcdefjhhhh
+#include "mainwindow.h"
+#include <QApplication>
+#include <QMessageBox>
+#include "connection.h"
+int main(int argc, char *argv[])
+{
+
+    QApplication a(argc, argv);
+    Connection c;
+    bool test=c.createconnect();
+    MainWindow w;
+
+    if(test)
+    {w.show();
+        QMessageBox::information(nullptr, QObject::tr("connection"),
+                    QObject::tr("connection successful.\n"
+                                "    Database Is Open"), QMessageBox::Ok);
+
+}
+    else
+        QMessageBox::critical(nullptr, QObject::tr("database is not open"),
+                    QObject::tr("connection failed.\n"), QMessageBox::Close);
+
+
+
+    return a.exec();
+}
